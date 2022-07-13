2fdd334c5c37e84da6ba726a7ec148897e48a632
Vika <zolina.viktoriya94@mail.ru>
2022-07-13 20:40:52 +0300

    Initial commit

---
+++ src/main/java/com/example/hello_my_bot/service/TelegramBot.java
@@ -0,0 +1,62 @@
+package com.example.hello_my_bot.service;
+
+import com.example.hello_my_bot.config.BotConfig;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+
+    final BotConfig config;
+
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+            switch (messageText) {
+                case "/start":
+                    startCommand(chatId, update.getMessage().getChat().getFirstName());
+               break;
+                default:sendMassage(chatId,"I'm sorry there is no such command");
+            }
+        }
+    }
+
+    private void startCommand(long chatId, String name) {
+        String answer = "Hi," + name + ", nice to meet you!";
+        sendMassage(chatId, answer);
+
+    }
+
+    private void sendMassage(long chatId, String textToSend) {
+        SendMessage massage = new SendMessage();
+        massage.setChatId(String.valueOf(chatId));
+        massage.setText(textToSend);
+
+        try {
+            execute(massage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/com/example/hello_my_bot/HelloMyBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello_my_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMyBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
