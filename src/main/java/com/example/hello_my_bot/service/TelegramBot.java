107d8e1f5b20a3a157669f8d726aabce45cd6e22
Vika <zolina.viktoriya94@mail.ru>
2022-07-15 09:52:32 +0300

    Keyboard

--- src/main/java/com/example/hello_my_bot/service/TelegramBot.java
+++ src/main/java/com/example/hello_my_bot/service/TelegramBot.java
@@ -5,11 +5,20 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.lang.reflect.Field;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Component
-public class TelegramBot extends TelegramLongPollingBot {
+public class TelegramBot extends TelegramLongPollingBot  {
 
 
     final BotConfig config;
@@ -29,22 +38,39 @@ public class TelegramBot extends TelegramLongPollingBot {
     }
 
     @Override
-    public void onUpdateReceived(Update update) {
+    public void onUpdateReceived(Update update)  {
         if (update.hasMessage() && update.getMessage().hasText()) {
-            String messageText = update.getMessage().getText();
+            String message = update.getMessage().getText();
             long chatId = update.getMessage().getChatId();
-            switch (messageText) {
+            sendMsg(chatId);
+            switch (message) {
                 case "/start":
                     startCommand(chatId, update.getMessage().getChat().getFirstName());
-               break;
-                default:sendMassage(chatId,"I'm sorry there is no such command");
+                    break;
+                case "/1":
+                    commandOne(chatId);
+                    return;
+                case "/d":
+
+                     return;
+                default:
+                    sendMassage(chatId, "Такой команды у меня нет.Вы можете воспользоваться командами /start , /1");
             }
         }
+
+
     }
 
     private void startCommand(long chatId, String name) {
-        String answer = "Hi," + name + ", nice to meet you!";
+        String answer = "Привет," + name + ", рад тебя видеть!";
         sendMassage(chatId, answer);
+
+
+    }
+
+    private void commandOne(long chatId) {
+        String one = "Ты выбрал команду один";
+        sendMassage(chatId, one);
 
     }
 
@@ -59,4 +85,31 @@ public class TelegramBot extends TelegramLongPollingBot {
             throw new RuntimeException(e);
         }
     }
+
+    public void sendMsg (long chatId) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(String.valueOf(chatId));
+        // Создаем клавиатуру
+        ReplyKeyboardMarkup replyKeyboardMarkup = new
+                ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(true);
+        // Создаем список строк клавиатуры
+        List<KeyboardRow> keyboard = new ArrayList<>();
+        // Первая строчка клавиатуры
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        // Добавляем кнопки в первую строчку клавиатуры
+        keyboardFirstRow.add("Команда 1");
+        keyboardFirstRow.add("Команда 2");
+        // Добавляем все строчки клавиатуры в список
+        keyboard.add(keyboardFirstRow);
+        // и устанавливаем этот список нашей клавиатуре
+        replyKeyboardMarkup.setKeyboard(keyboard);
+
+
+    }
 }
+
